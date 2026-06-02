7c5c415b813e92516a1877671c857ae2712f2830
victorbudunkevych <victor.budunkevych@gmail.com>
2026-06-02 15:02:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ from openpyxl.styles import Font, Alignment, PatternFill
 
 # ==================== PRODUCTION КОНФІГУРАЦІЯ ====================
 # PRODUCTION БОТ TOKEN
-TOKEN = "TOKEN = "8676112449:AAHu6vqV7JK4-8HZvOAM67e_23ZJFsSImqM"
+TOKEN = "8676112449:AAHu6vqV7JK4-8HZvOAM67e_23ZJFsSImqM"
 ADMIN_ID = [
     669706811,   # Віктор (власник)
     280240917,   # Шепшелей Владислав
